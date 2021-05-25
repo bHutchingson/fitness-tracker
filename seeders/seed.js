d91bad7f051ef5af2ca9ce4624622a48874b365f
Brandon <hutchingsonb@gmail.com>
2021-05-25 15:13:58 -0400

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const db = require('../models');
 
 mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
 });
 
 const workoutSeed = [
